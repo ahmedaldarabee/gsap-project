71eeee63a21d7c9807737cc287eb3b62a4417fd5
Ahmed Al Darabee <seahmednail@gmail.com>
2025-06-28 11:13:07 +0300

    add fonts and images to the project

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 
 function App() {
   return(
-    <div className='w-full h-screen flex items-center justify-center bg-gray-100'>
-      <h1 className='text-sky-600'>hello world</h1>
+    <div className='w-full min-h-screen flex items-center justify-center bg-gray-100'>
+      <h1 className='text-sky-600 text-3xl font-bold'>hello world</h1>
     </div>
   )
 }
