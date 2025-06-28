f9be54a2e3d48d94b16097eda138fde5e0c456a3
Ahmed Al Darabee <seahmednail@gmail.com>
2025-06-28 11:06:44 +0300

    initial to app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return(
+    <div className='w-full h-screen flex items-center justify-center bg-gray-100'>
+      <h1 className='text-sky-600'>hello world</h1>
+    </div>
+  )
+}
+
+export default App
