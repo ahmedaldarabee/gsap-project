65cc14b561f6de6f2683be493a7205d4d9d681b8
Ahmed Al Darabee <seahmednail@gmail.com>
2025-06-30 14:04:13 +0300

    end story section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,6 @@
+
+// reusable component
+
 const Button = ({title,id,rightIcon , leftIcon , containerClass}) => {
     return (
         <button id={id} className={`${containerClass} group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black`}>
